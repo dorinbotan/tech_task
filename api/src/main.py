22f5bf40b2293074f7b3b9e37e7045a4353c62b5
Dorin Botan <dorinbotan@mail.ru>
2021-11-04 02:28:32 +0000

    Query titles

--- api/src/main.py
+++ api/src/main.py
@@ -5,9 +5,29 @@ from typing import Optional
 api = FastAPI()
 
 @api.get("/api/titles")
-async def titles(title_class: Optional[str] = None, _sort: Optional[str] = None, _order: Optional[str] = None, _page: Optional[int] = None, _limit: Optional[int] = 100):
+async def titles(title_class: Optional[str] = None, _sort: Optional[str] = None, _order: Optional[str] = None, _page: Optional[int] = 0, _limit: Optional[int] = 100):
+    # TODO: ignore case
+    # Filter values by title_class
+    if title_class is not None:
+        filter = { "title_class": title_class }
+    else:
+        filter = {}
+
+    # Sort values by given key in the given order (by default order ascending by id)
+    sort = []
+    if _sort is not None:
+        _sort = _sort.split(',')
+        _order = _order.split(',')
+
+        for combination in zip(_sort, _order):
+            sort.append([combination[0], -1 if (combination[1] == 'desc') else 1])
+    else:
+        sort = [( "id", 1 )]
+
+    skip = _page * _limit
+
     titles = []
-    async for title in titles_collection.find({}, {"_id": 0, "id": 1, "title_number": 1, "title_class": 1}).limit(_limit):
+    async for title in titles_collection.find(filter, {"_id": 0, "content": 0}).sort(sort).skip(skip).limit(_limit):
         titles.append(title)
 
     return titles
