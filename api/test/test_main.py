83f9dfc48ed34e1a06e06f95d58a2a7ce876b62b
Dorin Botan <dorinbotan@mail.ru>
2021-11-09 11:10:58 +0000

    Test GET single title

--- api/test/test_main.py
+++ api/test/test_main.py
@@ -27,3 +27,15 @@ def test_get_titles_by_class():
         assert response_body[i]["title_class"] == "Freehold"
         assert "_id" not in response_body[i]
         assert "content" not in response_body[i]
+
+def test_get_single_title():
+    response = requests.get(relative_url("/api/titles/101"))
+    assert response.status_code == 200
+
+    response_body = response.json()
+
+    assert response_body["id"] == f'{101}'
+    assert response_body["title_number"] is not None
+    assert response_body["title_class"] in ["Freehold", "Leasehold"]
+    assert "_id" not in response_body
+    assert "content" is not None
