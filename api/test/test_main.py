430a3515c125f3e8da1628263c12420853c30347
Dorin Botan <dorinbotan@mail.ru>
2021-11-09 11:10:38 +0000

    Test filtering by title class (#2)
    * Ignore title_class case
    
    * Test filtering by title class

--- api/test/test_main.py
+++ api/test/test_main.py
@@ -4,3 +4,26 @@ from .helper import relative_url
 def test_get_titles():
     response = requests.get(relative_url("/api/titles"))
     assert response.status_code == 200
+
+    response_body = response.json()
+    assert len(response_body) == 100
+
+    for i in range(100):
+        assert response_body[i]["id"] == f'{i}'
+        assert response_body[i]["title_number"] is not None
+        assert response_body[i]["title_class"] in ["Freehold", "Leasehold"]
+        assert "_id" not in response_body[i]
+        assert "content" not in response_body[i]
+
+def test_get_titles_by_class():
+    response = requests.get(relative_url("/api/titles?title_class=freehold"))
+    assert response.status_code == 200
+
+    response_body = response.json()
+    assert len(response_body) == 100
+
+    for i in range(100):
+        assert response_body[i]["title_number"] is not None
+        assert response_body[i]["title_class"] == "Freehold"
+        assert "_id" not in response_body[i]
+        assert "content" not in response_body[i]
